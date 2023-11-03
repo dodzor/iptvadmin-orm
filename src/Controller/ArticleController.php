97638e82ca644938edd4348c4df45f24f326e83e
RAUL-DORIAN GHERGIC <raul.ghergic@rcs-rds.local>
2023-11-03 14:15:07 +0200

    test migrations + show article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,21 +4,51 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as ControllerAbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as AbstractController;
 
-class ArticleController extends ControllerAbstractController
+use App\Entity\Article;
+
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/", name="index", methods="GET")
      */
-    public function index()
+    public function index(EntityManagerInterface $entityManager)
     {
-        $articles = [
-            'Article 1',
-            'Article 2'
-        ];
+        $articles = $entityManager->getRepository(Article::class)->findAll();
+        // echo '<pre>';
+        // var_dump($articles); die();
 
         // return new Response('<html><body>Hello</body></html>');
         return $this->render('articles/index.html.twig', ['articles' => $articles]);
+    }
+
+    /** 
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function showId(EntityManagerInterface $entityManager, int $id) 
+    {
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        return $this->render('articles/show.html.twig', ['article' => $article]);
+    }
+
+    /**
+     * @Route("/article/save", name="save")
+     */
+    public function save(EntityManagerInterface $entityManager): Response
+    {
+        // $entityManager = $this->getDoctrine()->getManager();
+
+        $article = new Article();
+        $article->setTitle('Article two');
+        $article->setBody('This is the body of the article');
+
+        $entityManager->persist($article);
+
+        $entityManager->flush();
+
+        return new Response('Saved an article with id of '. $article->getId());
     }
 }
